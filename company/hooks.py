f0b4ce997b0307c36315f41cd059e2ea12e0bc79
hari <hariramu0404@gmail.com>
2026-06-06 15:35:15 +0800

    Reminder email send issues and Recipient default mail to user email

--- company/hooks.py
+++ company/hooks.py
@@ -134,13 +134,12 @@ doc_events = {
 
 
 scheduler_events = {
-    "cron": {
-        "* * * * *": [
-            "company.company.employee_remainder_api.check_and_enqueue_reminders",
-            "company.company.employee_remainder_api.process_remainder_queue",
-            "company.company.presence_api.process_auto_breaks"
-        ]
-    },
+    "all": [
+        "company.company.employee_remainder_api.check_and_enqueue_reminders",
+        "company.company.employee_remainder_api.process_remainder_queue",
+        "company.company.presence_api.process_auto_breaks",
+        "company.company.reminders.run_email_reminders"
+    ],
     "daily": [
         "company.company.api.update_expired_renewals",
         "company.company.presence_api.daily_reset",
